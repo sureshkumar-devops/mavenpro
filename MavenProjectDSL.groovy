0dd717646a20573db8883f9a3bd6b362e89cdd63
sureshkumar-devops <suresh.ak47@gmail.com>
2024-02-22 17:29:48 +0530

    added

---
+++ MavenProjectDSL.groovy
@@ -0,0 +1,16 @@
+job('FirstMaveJob-2024')
+{
+  description("First Maven Job gernerated..")
+  scm{
+    git("https://github.com/sureshkumar-devops/mavenpro.git", master)
+  }
+  trigger{
+    scm('* * * * * ')
+  }
+  steps{
+    maven('clean package','pom.xml')
+  }
+  publishers{
+    archiveArtifacts '**/*.jar'
+  }
+}
